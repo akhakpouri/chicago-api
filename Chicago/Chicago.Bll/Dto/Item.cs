9ec62ec1c51f62ecb192cc0b9fda275360824b3c
Ali Khakpouri <ali.khakpouri@gmail.com>
2021-07-25 22:51:23 -0400

    final changes

--- Chicago/Chicago.Bll/Dto/Item.cs
+++ Chicago/Chicago.Bll/Dto/Item.cs
@@ -12,5 +12,7 @@ namespace Chicago.Bll.Dto
         [Required]
         [Range(0.01, double.MaxValue, ErrorMessage = "Only positive numbers are allowd")]
         public int Type { get; set; }
+        public string TypeValue { get; set; }
+
     }
 }
